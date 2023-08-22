73b8fed0f321c46257253bf37018109d0f713193
Amir <natural6t@gmail.com>
2023-08-23 00:34:12 +0200

    Add cls method

--- src/Calendar.java
+++ src/Calendar.java
@@ -6,13 +6,30 @@ public class Calendar {
     private final Scanner scanner = new Scanner(System.in);
     private String userInput = "";
 
+    // worst clear console ever
+    private void cls(){
+        for (int i = 0; i <50; i++){
+            System.out.println();
+        }
+    }
+
     // main function to draw a screen.
     public void drawScreen(){
+        // updates(clears) console
+        cls();
+
         // prints ASCII label for current screen
         System.out.println(currentScreen.getAsciiLabel());
 
         switch (currentScreen){
             case MAIN:
+                // TODO: add custom rows
+                System.out.println("\n" +
+                        "1. Delete row.\n" +
+                        "2. Create new row.\n" +
+                        "3. Change order.\n" +
+                        "4. Change end date.\n" +
+                        "5. Exit.");
                 break;
             case DELETE_ROW:
                 break;
@@ -27,11 +44,14 @@ public class Calendar {
 
     // receives the userInput
     public void input(){
+        // input caret
+        System.out.print(">");
 
         // tabs, spaces check
         do{
             userInput = scanner.nextLine();
         } while (userInput.isBlank());
+
     }
 
     // main project logic
